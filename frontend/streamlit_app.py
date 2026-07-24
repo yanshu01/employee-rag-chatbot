eaeda48bf96ae61c3ee529a1f593eb68fe4b5446
yanshu01 <yatinsharma1007@gmail.com>
2026-07-24 13:45:12 +0530

    Fix Streamlit Cloud dependencies

--- frontend/streamlit_app.py
+++ frontend/streamlit_app.py
@@ -2,7 +2,10 @@ import requests
 import streamlit as st
 
 
-API_BASE_URL = "http://127.0.0.1:8000"
+API_BASE_URL = st.secrets.get(
+    "API_BASE_URL",
+    "http://127.0.0.1:8000",
+).rstrip("/")
 
 
 st.set_page_config(
